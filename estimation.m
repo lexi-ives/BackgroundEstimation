2214fec3ee772435ed20acd4b7340c781c23a786
Alexandra Ives <aei53f@mst.edu>
2018-11-29 12:45:48 -0600

    Read in sequence of images

--- estimation.m
+++ estimation.m
@@ -1,3 +1,13 @@
 close all;
 clear;
 
+D = 'images'; % directory where the files are saved
+S = dir(fullfile(D, 'simple_test_*.jpeg')); % pattern to match filenames
+N = numel(S); % number of images
+
+for k = 1:N
+    F = fullfile(D, S(k).name);
+    I = imread(F);
+    %figure, imshow(I);
+    S(k).data = I;
+end
