cbd64064d0c19f8070d1796cf02caf108f30c057
Alexandra Ives <aei53f@mst.edu>
2018-12-06 13:08:52 -0600

    Find mode cluster for each pixel

--- estimation.m
+++ estimation.m
@@ -3,24 +3,34 @@ clear;
 
 D = 'images'; % directory where the files are saved
 S = dir(fullfile(D, 'simple_test_*.jpeg')); % pattern to match filenames
-N = numel(S); % number of images
+N = numel(S); % number of images (frames)
+k = 5;
 
-for k = 1:N
+for f = 1:N
     % Read image, convert to grayscale
-    F = fullfile(D, S(k).name);
-    I = imread(F);
-    I = rgb2gray(double(I));
-    S(k).image = I;
+    F = fullfile(D, S(f).name);
+    Img = imread(F);
+    I = double(rgb2gray(Img));
+    S(f).image = I;
     %figure, imshow(I);
     
     % Perform k-means clustering
-    [idx, C] = kmeans(I, 5);
-    S(k).idx = idx; % cluster indeces
-    S(k).C = C; 	% cluster centroid locations
-
-    
+    clustered = reshape(kmeans(I(:), 5), size(I));
+    S(f).clustered = clustered; % cluster indeces
 end
 
-% perform clustering on each frame
+% determine mode cluster for each pixel
+[m,n] = size(S(1).image);
+modes = zeros(m,n);
+for i = 1:m
+    for j = 1:n
+        acc = zeros(k, 1);
+        for f = 1:N
+            acc(S(f).clustered(i,j)) = acc(S(f).clustered(i,j)) + 1;
+        end
+        [maxVal, maxIdx] = max(acc);
+        modes(i,j) = maxIdx;
+    end
+end
 
 
