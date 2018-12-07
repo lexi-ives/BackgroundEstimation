6157191140d7985ab6a6c9abb8f68bcf1952d522
Alexandra Ives <aei53f@mst.edu>
2018-12-07 16:18:34 -0600

    Gaussian filter before k-means, detects when there is no mode cluster

--- estimation.m
+++ estimation.m
@@ -1,20 +1,23 @@
 close all;
 clear;
 
-D = 'Landscapes/classroom'; % directory where the files are saved
+D = 'Landscapes/desert'; % directory where the files are saved
 S = dir(fullfile(D, '*.jpg')); % pattern to match filenames
 N = numel(S); % number of images (frames)
-k = 5;
+k = 5; % for k-means clustering
+sigma = 4;  % for gaussian blur
 
 for f = 1:N
     % Read image, convert to grayscale
     F = fullfile(D, S(f).name);
     Img = imread(F);
+    %Img = rgb2gray(Img);
     I = double(Img);
     S(f).image = I;
     %figure, imshow(I);
     
     % Perform k-means clustering
+    I = imgaussfilt(I, sigma);
     clustered = reshape(kmeans(I(:), 5), size(I));
     S(f).clustered = clustered; % cluster indeces
 end
@@ -29,7 +32,13 @@ for i = 1:m
             acc(S(f).clustered(i,j)) = acc(S(f).clustered(i,j)) + 1;
         end
         [maxVal, maxIdx] = max(acc);
-        modes(i,j) = maxIdx;
+        
+        z = size(find(acc == maxVal));
+        if (z(1) > 1)
+            modes(i,j) = 0;
+        else
+            modes(i,j) = maxIdx;
+        end
     end
 end
 
@@ -37,15 +46,19 @@ end
 estimated = zeros(m,n);
 for i = 1:m
     for j = 1:n
-        intensities = zeros(N, 1);
-        for f = 1:N
-            if S(f).clustered(i,j) == modes(i,j)
-               intensities(f) = S(f).image(i,j);
+        if modes(i,j) ~= 0 
+            intensities = zeros(N, 1);
+            for f = 1:N
+                if S(f).clustered(i,j) == modes(i,j)
+                   intensities(f) = S(f).image(i,j);
+                end
             end
+
+            estimated(i,j) = median(intensities(intensities ~=0));
+        else
+           estimated(i,j) = 0; 
         end
-        
-        estimated(i,j) = median(intensities(intensities ~=0));
     end
 end
 
-figure, imshow(uint8(estimated), 'InitialMagnification', 300);
+figure, imshow(uint8(estimated), 'InitialMagnification', 200);
