4733bd745c5b90cd1cbb5a44c8ec364f11eb18dc
Alexandra Ives <aei53f@mst.edu>
2018-12-11 12:08:28 -0600

    added better comments

--- estimation.m
+++ estimation.m
@@ -3,9 +3,9 @@ clear;
 
 % Delcare variables
 % -----------------
-D = 'images/brush'; % directory where the files are saved
-S = dir(fullfile(D, '*.jpg')); % pattern to match filenames
-N = numel(S); % number of images (frames)
+D = 'images/brush';             % directory where the files are saved
+S = dir(fullfile(D, '*.jpg'));  % pattern to match filenames
+N = numel(S);                   % number of images (frames)
 
 % Read images
 % -----------
@@ -28,29 +28,31 @@ for i = 1:k
     initialCenters(i) = i * partitionSize;
 end
 
-% Perform k-means clustering
-% --------------------------
+% Perform k-means clustering with Gaussian blur
+% ---------------------------------------------
 sigma = 4;  % for gaussian blur
 for f = 1:N
     I = imgaussfilt(I, sigma);
     [clustered, centroids] = kmeans(I(:), k, 'Start', initialCenters);
     clustered = reshape(clustered, size(I));
-    S(f).clustered = clustered; % cluster indeces
+    S(f).clustered = clustered;     % cluster indeces
 end
 
 % Determine mode cluster at each pixel
 % -------------------------------------
-modes = zeros(m,n);
+modes = zeros(m,n); % mode cluster indeces
 for i = 1:m
     for j = 1:n
+        % accumulate how many times each cluster occurs for this pixel
         acc = zeros(k, 1);
         for f = 1:N
             acc(S(f).clustered(i,j)) = acc(S(f).clustered(i,j)) + 1;
         end
-        [maxVal, maxIdx] = max(acc);
         
-        z = size(find(acc == maxVal));
-        if (z(1) > 1)
+        % determine if there is a single mode, if not set mode to 0
+        [maxVal, maxIdx] = max(acc);
+        numModes = size(find(acc == maxVal));
+        if (numModes(1) > 1)
             modes(i,j) = 0;
         else
             modes(i,j) = maxIdx;
@@ -63,9 +65,12 @@ end
 estimated = zeros(m,n);
 for i = 1:m
     for j = 1:n
+        % set pixel to 0 if it does not have a mode cluster
         if modes(i,j) == 0
             estimated(i,j) = 0;
         else
+            % build list of intensities for this pixel from each frame
+            % (if not in the mode cluster, will be set to 0)
             intensities = zeros(N, 1);
             for f = 1:N
                 if S(f).clustered(i,j) == modes(i,j)
@@ -73,6 +78,7 @@ for i = 1:m
                 end
             end
 
+            % find median of all pixels that belong to mode cluster 
             estimated(i,j) = median(intensities(intensities ~=0));            
         end
     end
