e1b216dc7fce1e833f93ed7fc102a5ed80c62376
kezipper <zipperer@gmail.com>
2018-12-13 12:56:16 -0600

    Added variable for number of frames to extract

--- extract_images.m
+++ extract_images.m
@@ -1,23 +1,15 @@
-% function images = extract_images(video_filepath, new_folder_filepath)
+% function images = extract_images(video_filepath, new_folder_filepath, num_frames)
 % Extract images - given a video file, return an array of still images 
 % uniformly extracted from the video
 
-
-% folder = 'new_folder_filepath';
-folder = 'images\memorial3';
-% vid = VideoReader(video_filepath);
-vid = VideoReader('videos/memorial_union3.mp4');
+num_frames = num_frames+1;
+folder = 'new_folder_filepath';
+vid = VideoReader(video_filepath);
 n = vid.NumberOfFrames;
 step = uint32(n/14);
 for k = step : step : n-1
     frames = read(vid,k);
     imwrite(frames, fullfile(folder, sprintf('%06d.jpg', k)));
 end
-FileList = dir(fullfile(folder, '*.jpg'));
-for iFile = 1:length(FileList)
-  aFile = fullfile(folder, FileList(iFile).name);
-  img   = imread(aFile);
-end
 
-% return aFile;
-% end
+end
